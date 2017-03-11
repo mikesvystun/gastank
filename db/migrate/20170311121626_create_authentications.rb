dabe073abab8947a35eec11c208dd3e22aa3af0d
Mike Svystun <michael.svystun@gmail.com>
2017-03-11 17:03:32 +0200

    added Facebook auth

---
+++ db/migrate/20170311121626_create_authentications.rb
@@ -0,0 +1,10 @@
+class CreateAuthentications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :authentications do |t|
+      t.string, :uid
+      t.string, :provider
+      t.belongs_to :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
