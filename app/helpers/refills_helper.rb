ccdfbed2a088f8bae48b1e2ab1b6fac56588c6ef
Mike <michael.svystun@gmail.com>
2017-05-17 22:55:03 +0300

    added overflow

--- app/helpers/refills_helper.rb
+++ app/helpers/refills_helper.rb
@@ -1,5 +1,4 @@
 module RefillsHelper
-
 def refill_row_class(refill)
   refill.full ? "full-row" : "" 
 end
@@ -9,5 +8,18 @@ def refill_edit_class(refill)
   refill.id != @car.refills.last.id ? "hidden" : "" 
 end
 
+end
+
+def overfill_edit_form_label(refill, attribute, text)
+
+  refill.id.nil? ? (label_tag attribute, text) : (hidden_field_tag attribute)
+end
+
+
+def overfill_edit_form(refill, attribute)
+
+  refill.id.nil? ? (text_field_tag attribute) : (hidden_field_tag attribute)
 
 end
+
+
