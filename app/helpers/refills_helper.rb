534f7dcc43c46adca086b7ee80ec9f56a30986dc
Mike <michael.svystun@gmail.com>
2017-05-18 21:38:33 +0300

    Fixed Sasha's comments

--- app/helpers/refills_helper.rb
+++ app/helpers/refills_helper.rb
@@ -9,17 +9,3 @@ def refill_edit_class(refill)
 end
 
 end
-
-def overfill_edit_form_label(refill, attribute, text)
-
-  refill.id.nil? ? (label_tag attribute, text) : (hidden_field_tag attribute)
-end
-
-
-def overfill_edit_form(refill, attribute)
-
-  refill.id.nil? ? (text_field_tag attribute) : (hidden_field_tag attribute)
-
-end
-
-
