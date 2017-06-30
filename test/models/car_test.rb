9fedb82db5605307d816eeb611b8d1d3f527bbee
Mike Svystun <mike@vertalab.com>
2017-06-30 11:55:12 +0300

    started with testing

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -4,4 +4,12 @@ class CarTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+test "should not save car without title" do
+  car = Car.new
+  assert_not car.save,  "Saved the car without a title"
+end
+
+
 end
